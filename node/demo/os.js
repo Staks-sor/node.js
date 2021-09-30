2e921e3545f5fabddbe1f4ff6f52d5e1ce01e14c
Stanislav <stas.sor@gmail.com>
2021-09-30 13:00:17 +0300

    добавил в os.js

--- node/demo/os.js
+++ node/demo/os.js
@@ -1,3 +1,14 @@
 const os = require('os')
+const chalk = require('chalk')
 
-console.log('One')
+console.log(chalk.red('Операционаая система: ', os.platform()))
+
+console.log(chalk.green('Архитектура процессора: ', os.arch()))
+
+console.log('Информация по процессорам: ', os.cpus())
+
+console.log(chalk.blue('Свободная память: ', os.freemem()))
+
+console.log(chalk.blue('Всего памяти: ', os.totalmem()))
+
+console.log(chalk.red('домашняя директория: ', os.homedir()))
