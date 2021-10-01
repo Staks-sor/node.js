11ddbf11673e24add5fbe1afcc4ff46e2be49990
Stanislav <stas.sor@gmail.com>
2021-10-01 15:07:48 +0300

    index.js update http

--- node/index.js
+++ node/index.js
@@ -1,7 +1,9 @@
-const chalk = require('chalk')
-const text = require('./data.js')
+// const chalk = require('chalk')
+// const text = require('./data.js')
+//
+// console.log(chalk.yellow(text))
+//
+// console.log(chalk.blue(__dirname))
+// console.log(chalk.red(__filename))
 
-console.log(chalk.yellow(text))
-
-console.log(chalk.blue(__dirname))
-console.log(chalk.red(__filename))
+const http = require('http')
