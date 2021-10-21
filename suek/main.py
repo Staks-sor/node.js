045ac9390cc71e7939db0b0462403adb627fec86
Stanislav <stas.sor@gmail.com>
2021-10-21 12:03:54 +0300

    чет не то

--- suek/main.py
+++ suek/main.py
@@ -11,6 +11,4 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-3213мчсмчсмчсмч
+    print_hi('PyChar3423424234 32кцуапмт ка
