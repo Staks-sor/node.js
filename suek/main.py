14dd94cba411913989bc4d69305f9482b6f56261
Stanislav <stas.sor@gmail.com>
2021-10-21 12:05:04 +0300

    чет не том счм

--- suek/main.py
+++ suek/main.py
@@ -11,4 +11,4 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyChar3423424234 32кцуапмт ка
+    print_hi('PyChar3423424234 32кчсм чсмцуапмт ка
