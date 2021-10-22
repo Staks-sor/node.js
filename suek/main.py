cc7366c68310525052672aecd36436bf32815768
Stanislav <stas.sor@gmail.com>
2021-10-22 12:22:48 +0300

    Продолжаю учиться, ну всвязи с плохим самочувствие, дело совсем не идет( исправил ошибку в слове. добавил ввод табеля

--- suek/main.py
+++ suek/main.py
@@ -1,1 +1,18 @@
-123
+import sqlite3
+
+db = sqlite3.connect('server.db')
+sql = db.cursor()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS users (
+    login TEXT,
+    password TEXT,
+    cash BIGINT,
+)""")
+
+db.commit()
+
+user_login = input('Login:')
+user_password = input('Password:')
+
+sql.execute("SELECT login FROM users")
+if sql.fetchone() is None
